1fd4f16a7edc3ec115c24cb7e126b929cf116bb3
Eray Ates <72857634+erayates@users.noreply.github.com>
2023-07-24 00:55:01 +0300

    Updated a bug for production.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -13,7 +13,7 @@ export default function Contact() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Let's Keep In Touch</h1>
+        <h1 className={styles.title}>Lets Keep In Touch</h1>
         <form className={styles.form}>
           <input type="text" placeholder="name" className={styles.input} />
           <input type="text" placeholder="email" className={styles.input} />
